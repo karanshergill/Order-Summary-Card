f24134db7148ae80af8bcdfde45590a784aff9e6
Karan <email@karanshergill.com>
2024-07-23 11:26:15 +0530

    fixed css

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*.html'],
+  theme: {
+    extend: {
+      colors: {
+        'Primary-Pale-Blue': 'hsl(225, 100%, 94%)',
+        'Primary-Bright-Blue': 'hsl(245, 75%, 52%)',
+        'Neutral-Pale-Blue': 'hsl(225, 100%, 98%)',
+        'Neutral-Desaturated-Blue': 'hsl(224, 23%, 55%)',
+        'Neutral-Dark-Blue':'hsl(223, 47%, 23%)'
+      },
+      fontFamily: {
+        'Red-Hat-Display': ["Red Hat Display"]
+      }
+    },
+  },
+  plugins: [],
+}
+
+
+
